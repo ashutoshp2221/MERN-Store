00917a814f4af09c167e8c8b95cb42497cc8d087
Ashutosh Parmar <59583249+ashutoshp2221@users.noreply.github.com>
2022-01-29 16:21:27 +0530

    project frontend

---
+++ projfrontend/src/App.js
@@ -0,0 +1,11 @@
+// You don't need app.js cuz we will be throwing on index.js
+// not even exporting it
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
